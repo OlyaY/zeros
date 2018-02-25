169d090844dbd40a06dbf97e29a1bd43fc005de1
OlyaY <olga.yukhnovets@gmail.com>
2018-02-26 02:21:32 +0300

    zeros/feature: implement  function. test 100/100

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var number;
+  var result = 0;
+  while (number > 0) {
+    number = parseInt(number / 5);
+    result = parseInt(result + number);
+  }
+  return result;
 }
